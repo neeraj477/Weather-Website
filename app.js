a0b95879bda36cefe2a5f28f9a3b750b8fc46b28
neeraj477 <neerajmahajan169@gmail.com>
2024-07-16 13:15:43 +0530

    Main Content

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ let intWindSpeed;
 let intPressuremSl;
 let upcomingDays;
 let nextDays;
+let maxdailyTemp=[];
+let intMaxDailyTemp=[];
+let mindailyTemp=[];
+let intMinDailyTemp=[];
+let windSpeed=[];
+let intDailyWindSpeed=[];
 
 const inputElement = document.querySelector("#cityInput");
 inputElement.addEventListener('keydown', function(event) {
@@ -39,7 +45,7 @@ const getLocation = async() => {
         console.log(longitude);
         console.log(nextDays);
         },5000);
-    url2 = `https://api.open-meteo.com/v1/forecast?latitude=${longitude}&longitude=${latitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,pressure_msl,wind_speed_10m&daily=weather_code,temperature_2m_max,precipitation_probability_max,wind_speed_10m_max&timeformat=unixtime&timezone=Asia%2FTokyo`;
+    url2 = `https://api.open-meteo.com/v1/forecast?latitude=${longitude}&longitude=${latitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,pressure_msl,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max,wind_speed_10m_max&timeformat=unixtime&timezone=Asia%2FTokyo`;
     await getWeather();
 };
 
@@ -52,14 +58,21 @@ const getWeather = async() => {
     let currentTemp = await data2.current.temperature_2m;
     let apparentTemp = await data2.current.apparent_temperature;
     humidity = await data2.current.relative_humidity_2m;
-    let windSpeed = await data2.current.wind_speed_10m;
+    let currentWindSpeed = await data2.current.wind_speed_10m;
     let pressure = await data2.current.pressure_msl;
+    maxdailyTemp = await data2.daily.temperature_2m_max;
+    mindailyTemp = await data2.daily.temperature_2m_min;
+    windSpeed = await data2.daily.wind_speed_10m_max;
     unixTime = await data2.current.time;
     intCurrentTemp = Math.floor(currentTemp);
     intApparentTemp = Math.floor(apparentTemp);
-    intWindSpeed = Math.floor(windSpeed);
+    intWindSpeed = Math.floor(currentWindSpeed);
     intPressuremSl = Math.floor(pressure);
-    console.log(pressure);
+    for(let i=0; i<6; i++){
+        intMaxDailyTemp[i] = Math.floor(maxdailyTemp[i]);
+        intMinDailyTemp[i] = Math.floor(mindailyTemp[i]);
+        intDailyWindSpeed[i] = Math.floor(windSpeed[i]);}
+    console.log(windSpeed);
     getDay();
     changeText();
 };
@@ -93,23 +106,41 @@ function changeText() {
     windSpeed.innerText = `${intWindSpeed}km/h`; 
     const seaPressure = document.querySelector("#pressureSealvl");
     seaPressure.innerText = `${intPressuremSl}hPa`;
-    const day12 = document.querySelector("#day1");
-    day12.innerText=`${nextDays[0]}`;
-    console.log(nextDays[0]);
+    const day1 = document.querySelector("#day1");
+    day1.innerText=`${nextDays[0]}`;
     const day2 = document.querySelector("#day2");
     day2.innerText=`${nextDays[1]}`;
-    console.log(nextDays[1]);
     const day3 = document.querySelector("#day3");
     day3.innerText=`${nextDays[2]}`;
-    console.log(nextDays[2]);
     const day4 = document.querySelector("#day4");
     day4.innerText=`${nextDays[3]}`;
-    console.log(nextDays[3]);
     const day5 = document.querySelector("#day5");
     day5.innerText=`${nextDays[4]}`;
-    console.log(nextDays[4]);
     const day6 = document.querySelector("#day6");
     day6.innerText=`${nextDays[5]}`;
-    console.log(nextDays[5]);
+    const tempDay1 = document.querySelector("#temp1");
+    tempDay1.innerText=`${intMinDailyTemp[0]}-${intMaxDailyTemp[0]}°C`;
+    const tempDay2 = document.querySelector("#temp2");
+    tempDay2.innerText=`${intMinDailyTemp[1]}-${intMaxDailyTemp[1]}°C`;
+    const tempDay3 = document.querySelector("#temp3");
+    tempDay3.innerText=`${intMinDailyTemp[2]}-${intMaxDailyTemp[2]}°C`;
+    const tempDay4 = document.querySelector("#temp4");
+    tempDay4.innerText=`${intMinDailyTemp[3]}-${intMaxDailyTemp[3]}°C`;
+    const tempDay5 = document.querySelector("#temp5");
+    tempDay5.innerText=`${intMinDailyTemp[4]}-${intMaxDailyTemp[4]}°C`;
+    const tempDay6 = document.querySelector("#temp6");
+    tempDay6.innerText=`${intMinDailyTemp[5]}-${intMaxDailyTemp[5]}°C`;
+    const windSpeed1 = document.querySelector("#windSpeed1");
+    windSpeed1.innerText=`${intDailyWindSpeed[0]}km/h`;
+    const windSpeed2 = document.querySelector("#windSpeed2");
+    windSpeed2.innerText=`${intDailyWindSpeed[1]}km/h`;
+    const windSpeed3 = document.querySelector("#windSpeed3");
+    windSpeed3.innerText=`${intDailyWindSpeed[2]}km/h`;
+    const windSpeed4 = document.querySelector("#windSpeed4");
+    windSpeed4.innerText=`${intDailyWindSpeed[3]}km/h`;
+    const windSpeed5 = document.querySelector("#windSpeed5");
+    windSpeed5.innerText=`${intDailyWindSpeed[4]}km/h`;
+    const windSpeed6 = document.querySelector("#windSpeed6");
+    windSpeed6.innerText=`${intDailyWindSpeed[5]}km/h`;
 }
 
